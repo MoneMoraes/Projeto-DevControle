a502bdfc25737dc76e2a432f97b851ba15680792
DESKTOP-5JL8LUQ\Ramon Moraes <MoneMoraes>
2025-02-17 18:23:04 -0300

    realizada estilização da pagina home, configuração de conexão com o banco de dados MongoDb, e autenticacção com o gmail.

---
+++ devcontrole/src/app/api/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/container"
+
+export default function Dashboard(){
+    return (
+        <div>
+            <Container>
+                <h1>Página dashboard</h1>
+            </Container>
+        </div>
+    )
+}
---
+++ devcontrole/src/components/container/index.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+export function Container({ children }:{ children : ReactNode }){
+    return(
+        <div className='w-full max-w-7xl max-auto'>
+            {children}
+        </div>
+    )
+}
